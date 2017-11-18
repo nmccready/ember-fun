4f8535df6ee8f7df8da75dd8efe7e6643d7aab47
nmccready <nemtcan@gmail.com>
2017-11-17 19:09:53 -0500

    authentication service working... had to restart ember s.. sheesh

--- github-ui/app/routes/org.js
+++ github-ui/app/routes/org.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // authentication: Ember.inject.service(),
+  authentication: Ember.inject.service(),
   
-  // setupController(ctrl) {
-  //   this._super(...arguments);
-  //   ctrl.set('records', this.get('authentication.records'));
-  // }
+  setupController(ctrl) {
+    this._super(...arguments);
+    ctrl.set('records', this.get('authentication.records'));
+  }
 });
