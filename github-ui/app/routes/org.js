6a7fe5dab7acf594155fd42b612f0011b04cdbcb
nmccready <nemtcan@gmail.com>
2017-11-20 10:12:54 -0500

    addToRecords is working

--- github-ui/app/routes/org.js
+++ github-ui/app/routes/org.js
@@ -1,10 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  
   authentication: Ember.inject.service(),
   
   setupController(ctrl) {
     this._super(...arguments);
     ctrl.set('records', this.get('authentication.records'));
+  },
+
+  actions: {
+    addToRecords(val) {
+      this.get('authentication.records').addObject({id: val});
+    }
   }
 });
